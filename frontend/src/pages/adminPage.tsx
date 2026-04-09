1d006f8ac5ce8a3949efe037ad71d6993ab23072
Testiing001 <2018soulmortal@gmail.com>
2026-04-09 01:54:09 +0000

    feat: delete all function

--- frontend/src/pages/adminPage.tsx
+++ frontend/src/pages/adminPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Upload, Trash2, Tag, LogOut } from "lucide-react";
+import { Upload, Trash2, Tag, LogOut, X } from "lucide-react";
 
 const BASE_URL = "https://studious-enigma-77wp5pqj6v63xxqj-8000.app.github.dev";
 
@@ -22,7 +22,6 @@ export const AdminPage = () => {
     const [error, setError] = useState("");
     const fileInputRef = useRef<HTMLInputElement>(null);
 
-    // Fetch images on load
     useEffect(() => {
         const fetchImages = async () => {
             try {
@@ -42,7 +41,6 @@ export const AdminPage = () => {
         fetchImages();
     }, []);
 
-    // Upload images to backend
     const handleUploadImages = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
         if (!files) return;
@@ -58,7 +56,6 @@ export const AdminPage = () => {
                 },
             });
 
-            // Refresh images after upload
             const res = await axios.get(`${BASE_URL}/admin/view/`, {
                 headers: getAuthHeaders(),
             });
@@ -75,7 +72,6 @@ export const AdminPage = () => {
         e.target.value = "";
     };
 
-    // Delete selected images
     const handleDelete = async () => {
         if (selected.length === 0) return;
 
@@ -103,15 +99,25 @@ export const AdminPage = () => {
         );
     };
 
+    const isAllSelected = images.length > 0 && selected.length === images.length;
+
+    const handleSelectAllChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            const allIds = images.map(img => img.id);
+            setSelected(allIds);
+        } 
+        else    setSelected([]);
+    };
+
     const handleLogout = () => {
         localStorage.removeItem("token");
         navigate("/adminlogin");
     };
 
     return (
-        <div className="min-h-screen bg-gray-900 text-white pt-4 px-8">
+        <div className="min-h-screen bg-black/70 text-white px-10 pt-3">
             <div className="flex items-center justify-between mb-4">
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4 mt-1">
                     <button className="flex items-center gap-2 px-4 py-2 cursor-pointer bg-indigo-600 hover:bg-indigo-500 rounded-lg transition">
                         <Tag size={16} />Face Category
                     </button>
@@ -119,22 +125,14 @@ export const AdminPage = () => {
                     <button onClick={() => fileInputRef.current?.click()} className="flex items-center gap-2 px-4 py-2 cursor-pointer bg-green-600 hover:bg-green-500 rounded-lg transition">
                         <Upload size={16} />Upload Photos
                     </button>
-                    <input ref={fileInputRef} type="file" multiple accept="image/*" className="hidden"
-                        onChange={handleUploadImages}
-                    />
+                    <input ref={fileInputRef} type="file" multiple accept="image/*" className="hidden" onChange={handleUploadImages}/>
 
-                    <button
-                        onClick={() => { setDeleteMode(!deleteMode); setSelected([]); }}
-                        className="flex items-center gap-2 px-4 py-2 cursor-pointer rounded-lg transition bg-red-700 hover:bg-red-600"
-                    >
-                        <Trash2 size={16} />{deleteMode ? "Cancel" : "Delete Photos"}
+                    <button onClick={() => { images.length > 0 && setDeleteMode(!deleteMode); setSelected([]); }} className="flex items-center gap-2 px-4 py-2 cursor-pointer rounded-lg transition bg-red-600 hover:bg-red-500">
+                        {deleteMode ? <><X size={16} /> Cancel</> : <><Trash2 size={16} /> Delete Photos</>}
                     </button>
 
                     {deleteMode && selected.length > 0 && (
-                        <button
-                            onClick={handleDelete}
-                            className="flex items-center gap-2 px-4 py-2 cursor-pointer rounded-lg transition bg-red-500 hover:bg-red-400 font-semibold"
-                        >
+                        <button onClick={handleDelete} className="flex items-center gap-2 px-4 py-2 cursor-pointer rounded-lg transition bg-red-500 hover:bg-red-400 font-semibold">
                             Confirm Delete ({selected.length})
                         </button>
                     )}
@@ -147,32 +145,25 @@ export const AdminPage = () => {
 
             {error && <p className="mb-4 text-red-400 font-semibold">{error}</p>}
 
-            {deleteMode && (
-                <p className="mb-4 text-red-400 font-semibold">
-                    Select images to delete
-                </p>
-            )}
+            <div className="w-[95%] mx-auto h-4 mb-4">
+                {deleteMode && (
+                    <div className="flex justify-between mx-3 mb-4">
+                        <p className="text-red-400 font-semibold">Select images to delete</p>
+                        <div className="flex justify-center items-center gap-2">
+                            <p className="text-red-400 font-semibold">Select All</p>
+                            <input type="checkbox" checked={isAllSelected} onChange={handleSelectAllChange} className="w-4 h-4 accent-red-500" />
+                        </div>
+                    </div>
+                )}
+            </div>
 
             {images.length > 0 ? (
-                <div className="grid grid-cols-4 gap-4">
+                <div className="w-[95%] mx-auto grid grid-cols-4 gap-4">
                     {images.map((img) => (
-                        <div
-                            key={img.id}
-                            className={`relative border rounded-lg overflow-hidden ${selected.includes(img.id) ? "border-red-500 ring-2 ring-red-500" : "border-gray-700"}`}
-                        >
-                            <img
-                                src={img.image}
-                                alt={`${img.id}`}
-                                className="w-full h-48 object-contain"
-                            />
-
+                        <div key={img.id} className="relative border rounded-lg border-gray-700">
+                            <img src={img.image} alt={`${img.id}`} className="aspect-square object-cover"/>
                             {deleteMode && (
-                                <input
-                                    type="checkbox"
-                                    className="absolute top-2 left-2 w-5 h-5 accent-red-500"
-                                    checked={selected.includes(img.id)}
-                                    onChange={() => toggleSelect(img.id)}
-                                />
+                                <input type="checkbox" className="absolute top-2 right-2 w-5 h-5 accent-red-500" checked={selected.includes(img.id)} onChange={() => toggleSelect(img.id)}/>
                             )}
                         </div>
                     ))}
