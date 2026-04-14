15f079b95cf4a4a5ed4c2cf2f9e7723024e99217
AdityaSinghWebDev <adityasingh830385@gmail.com>
2026-04-14 15:51:50 +0000

    allow all origin

--- backend/app/middleware/middleware.py
+++ backend/app/middleware/middleware.py
@@ -2,12 +2,10 @@ import os
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
-origins = os.getenv("ALLOWED_ORIGINS").split(",")
-
 def add_middlewares(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
